f434a6ed6c3e752a94d6c1b17ce0bf0ca2ffb2b7
roboslyq <robosloveu@163.com>
2020-04-25 23:01:17 +0800

    将redis改为cmake管理，默认是make管理

---
+++ deps/hiredis/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/d/IdeaProjects/redis/deps/hiredis
+# Build directory: /cygdrive/d/IdeaProjects/redis/deps/hiredis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "/cygdrive/d/IdeaProjects/redis/deps/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/d/IdeaProjects/redis/deps/hiredis/CMakeLists.txt;83;ADD_TEST;/cygdrive/d/IdeaProjects/redis/deps/hiredis/CMakeLists.txt;0;")
